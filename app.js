ceb43a56b4ad54ecf0c66489f09c0434bf265890
Happiness Balogun <happinessblgn0@gmail.com>
2019-11-10 10:37:27 +0100

    Updates

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ function heroClassToggle() {
   var element = document.getElementById("hero");
   element.classList.toggle("toggle-hero");
 }
+
+// FAQ PAGE
+function toggleIcon(e) {
+    $(e.target)
+      .prev(".panel-heading")
+      .find(".more-less")
+      .toggleClass("fa-sort-up fa-sort-down");
+  }
+  $(".panel-group").on("hidden.bs.collapse", toggleIcon);
+  $(".panel-group").on("shown.bs.collapse", toggleIcon);
